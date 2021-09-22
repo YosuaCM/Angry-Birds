2f57628e8f4186c28f518d66daecbf57c08041e5
YosuaCM <90274542+YosuaCM@users.noreply.github.com>
2021-09-22 23:02:22 +0700

    Dilo Angry Bird

---
+++ Angry Birds/Assets/Scripts/ExplodeBird.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeBird : Bird
+{
+    public float Impact;
+
+    public float force;
+
+    public LayerMask LayerToHit;
+
+    public GameObject ExplosionEffect;
+
+
+    public override void OnCollisionEnter2D(Collision2D col)
+    {
+        explode();
+    }
+
+    void explode()
+    {
+        Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, Impact, LayerToHit);
+
+        foreach(Collider2D obj in objects)
+        {
+            Vector2 direction = obj.transform.position - transform.position;
+
+            obj.GetComponent<Rigidbody2D>().AddForce(direction * force);
+
+        }
+
+        //GameObject ExplosionEffectIns = Instantiate(ExplosionEffect, transform.position, Quaternion.identity);
+        //Destroy(ExplosionEffectIns, 10);
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, Impact);
+
+    }
+}
---
+++ Angry Birds/Assets/Scripts/Menu_Option.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Option : MonoBehaviour
+{
+    public void Scene_1()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+
+    public void Scene_2()
+    {
+        SceneManager.LoadScene("Level_2");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
